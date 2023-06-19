5f5a20fdb6b21adab5b89cdc357e3dd00c0cf3fc
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-19 12:13:41 -0300

    Desafios e Revisao

---
+++ desafio/loja.js
@@ -0,0 +1,35 @@
+const leia=require("prompt-sync")()
+
+let codigo = ["p01","p02","p03","p04"]
+let produtos = ["camisa","calça","saia","short"]
+let preco = [0,0,0,0]
+
+console.log("COD\tPRODUTO\tPREÇO")
+for(let i=0; i<codigo.length; i++){
+    console.log(codigo[i]+'\t'+produtos[i]+'\t'+preco[i])
+}
+
+console.log("A digitação de valores")
+for(let i=0; i<codigo.length; i++){
+    console.log(codigo[i]+'\t'+produtos[i])
+    preco[1] = leia("Digite o valor do produto")
+}
+
+console.log("COD\tPRODUTO\tPREÇO")
+for(let i=0; i<codigo.length; i++){
+    console.log(codigo[i]+'\t'+produtos[i]+'\t'+preco[i])
+}
+
+console.log("Alteração de Dados")
+let codigos = leia("Digite o codigo do produto que deseja trocar o nome: ")
+for(let i=0; i<codigo.length; i++){
+    if(codigos == codigo[i]){
+        console.log("Nome atual do produto: "+produtos[i])
+        produtos[i] = leia("Digite o novo nome: ")
+    }
+}
+
+console.log("COD\tPRODUTO\tPREÇO")
+for(let i=0; i<codigos.length; i++){
+    console.log(codigos[i]+'\t'+produtos[i]+'\t'+preco[i])
+}
